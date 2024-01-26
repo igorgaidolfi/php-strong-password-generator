7710d1fed203a02886990f217086b2beb33933cf
igorgaidolfi <gandalf.4@outlook.it>
2024-01-26 17:37:35 +0100

    added logic of Milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
+<?php
+    $num = rand(1,18);
+    $al= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $an= '0123456789';
+    $as= '!$%&()=.?';
+    $apw= '';
+
+    // if(strlen($apw) < $num){
+    //     $apw= substr(str_shuffle($al),0,1);
+    // }
+    // var_dump($apw);
+    // var_dump(strlen($apw));
+    // $apw.=substr(str_shuffle($al),0,1);
+    // var_dump($apw);
+    // var_dump(strlen($apw));
+
+    //     die();
+    while(strlen($apw)<$num){
+        if(strlen($apw) < $num){
+            $apw.= substr(str_shuffle($al),0,1);
+        }
+        if(strlen($apw) < $num){
+            $apw.=rand(0,9);
+        }
+        if(strlen($apw) < $num){
+            $apw.=substr(str_shuffle($al),0,1);
+        }
+    }
+    var_dump($apw);
+            // $count = 3;
+            // $str = str_replace("abcdefghijklmnopqrstuvwxyz", "", "ciao", $count);
+            // var_dump(stristr('01123abcdefghijklmnopqrstuvwxyz456789', 'g'));
+            // var_dump("/[0-9]+/");
+        ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,6 +42,5 @@
         <title>Random Password Generator</title>
     </head>
     <body>
-        
     </body>
 </html>
